d8be176bdbc945373c8ea8dbeada2ff6e91965a1
Marco Primi <marco@mpri.me>
2022-05-20 11:24:39 -0700

    Simple file upload and download

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # nasefa
 ## Send and receive files using NATS
+
+## Usage:
+
+`$ nasefa send tax-return.pdf`
+
+`$ nasefa recv tax-return.pdf ~/Desktop`
--- nasefa.go
+++ nasefa.go
@@ -2,8 +2,40 @@ package main
 
 import (
   "log"
+  "os"
+  "nasefa/helpers"
 )
 
+func send(sourceFile string)  {
+  err := helpers.UploadFile(sourceFile)
+  if err != nil {
+    log.Printf("File upload failed: %s", err)
+    os.Exit(1)
+  }
+}
+
+func recv(fileId, destinationDirectory string)  {
+  err := helpers.DownloadFiles(fileId, destinationDirectory)
+  if err != nil {
+    log.Printf("Files download failed: %s", err)
+    os.Exit(1)
+  }
+}
+
 func main()  {
-  log.Printf("Hello World!")
+  if len(os.Args) < 3 {
+    log.Printf("Usage:")
+    log.Printf(" nasefa send <file>")
+    log.Printf(" nasefa recv <file-id> <directory>")
+    os.Exit(1)
+  }
+
+  if os.Args[1] == "send" {
+    send(os.Args[2])
+  } else if os.Args[1] == "recv" {
+    recv(os.Args[2], os.Args[3])
+  } else {
+    log.Printf("Invalid options")
+    os.Exit(1)
+  }
 }
